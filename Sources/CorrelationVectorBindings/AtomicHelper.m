08d19923f73ed5b68c724ca63cd4df9f209bb337
Ivan Matkov <v-ivmatk@microsoft.com>
2019-07-09 12:17:54 +0300

    Drop atomics and bindings submodule and use locks instead

--- Sources/CorrelationVectorBindings/AtomicHelper.m
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-#import "AtomicHelper.h"
-
-bool compareAndSwap(_Atomic(uint32_t) *value, uint32_t *expected, uint32_t desired) {
-  return atomic_compare_exchange_strong(value, expected, desired);
-}
